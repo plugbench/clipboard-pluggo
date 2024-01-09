e7c82b8aa50a7ce8a8ed5f353995440d952b80f7
Jason Felice <jason.m.felice@gmail.com>
2024-01-09 11:40:02 -0500

    Bump flakes

--- derivation.nix
+++ derivation.nix
@@ -16,7 +16,7 @@ buildGoModule {
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = lib.optional stdenv.isDarwin Cocoa ++ lib.optional (!stdenv.isDarwin) libX11;
 
-  vendorSha256 = "OmtzQfirsDnR31vIy1nwrm0L3t83BvFyOJcCXsWroRw=";
+  vendorHash = "sha256:OmtzQfirsDnR31vIy1nwrm0L3t83BvFyOJcCXsWroRw=";
 
   postInstall = lib.optional (!stdenv.isDarwin) ''
     wrapProgram $out/bin/clipboard \
